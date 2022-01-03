7e4ca5f1c761420402ced732ccfc2c0aed619f10
w0771056 <w0771056@myscc.ca>
2022-01-03 15:44:22 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReachingfortheStarsOptimist.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
